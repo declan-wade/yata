9456468358afd12565097b02ccb7d5be2b74de17
Declan Wade <17171018+declan-wade@users.noreply.github.com>
2025-05-21 12:17:36 +0800

    Fixed TS error

--- app/tag/[tag]/page.tsx
+++ app/tag/[tag]/page.tsx
@@ -3,11 +3,7 @@ import { getAllTags, getFilteredTodo } from "@/lib/database";
 import TodoDashboard from "@/components/todo-dashboard";
 import { Todo, TodoTag } from "@/lib/types";
 
-interface PageProps {
-  params: { tag: string };
-}
-
-export default async function Home({ params }: PageProps) {
+export default async function Home({ params }: any) {
   // Fetch data on the server using the dynamic tag param
   const todos: Todo[] = await getFilteredTodo(params.tag);
   const tags: TodoTag[] = await getAllTags();
